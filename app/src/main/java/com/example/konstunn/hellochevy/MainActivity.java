2bb739266d587fe070ee3e384741837569234521
Konstantin Gorbunov <konstunn@users.noreply.github.com>
2016-10-20 22:28:19 +0700

    Set textView text on image button click

--- app/src/main/java/com/example/konstunn/hellochevy/MainActivity.java
+++ app/src/main/java/com/example/konstunn/hellochevy/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.konstunn.hellochevy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mHelloTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // вы должны были запомнить идентификатор
+        mHelloTextView = (TextView)findViewById(R.id.textView);
+    }
+
+    public void onClick(View view) {
+        mHelloTextView.setText("Hello Chevy!");
     }
 }
