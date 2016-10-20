84a416161cd34dc730ff53608459f71ff9705f4c
Konstantin Gorbunov <konstunn@users.noreply.github.com>
2016-10-20 23:58:45 +0700

    Count chevies

--- app/src/main/java/com/example/konstunn/hellochevy/MainActivity.java
+++ app/src/main/java/com/example/konstunn/hellochevy/MainActivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mInfoTextView;
     private EditText mNameEditText;
+
+    private Button mCheviesCountButton;
+    private int mCheviesCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
 
         mInfoTextView = (TextView)findViewById(R.id.textView);
         mNameEditText = (EditText) findViewById(R.id.editText);
+        mCheviesCountButton = (Button) findViewById(R.id.cheviesCountButton);
+
+        mCheviesCountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mInfoTextView.setText("I've counted " + ++mCheviesCount + "Chevies");
+            }
+        });
     }
 
     public void onClick(View view) {
